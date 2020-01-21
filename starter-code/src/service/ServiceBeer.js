f4ae4499579e88692a358122d40b66c596b30835
juanorro <juan.rodriguezjim@gmail.com>
2020-01-21 19:53:17 +0100

    service created

---
+++ starter-code/src/service/ServiceBeer.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: 'https://github.com/juanorro/lab-react-ironbeers.git'
+});
+
+export const getBeers = () => http.get('/').then(res => res.data);
+export const getOneBeers = (id) => http.get(`/${id}`).then(res => res.data);
+export const getRandomBeer = () => http.get(`/random`).then(res => res.data);
+export const postNewBeer = () => http.post(`/new`).then(res => res.data);
+export const getSearchBeer = (query) => http.get(`/search?q=${query}`).then(res => res.data);
+
+export default {
+    http
+}
