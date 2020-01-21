69177b12cd473035d827f1476ac587d7245323dd
Aitor Arina <aitorulz1@gmail.com>
2020-01-21 20:25:23 +0100

    Juanorro clone

--- starter-code/src/components/New.js
+++ starter-code/src/components/New.js
@@ -7,8 +7,8 @@ const New = () => {
             <div className="">
                     <img src="../images/new-beer.png" alt="" />
                     <div className="text-container">
-                        <div>New BEER</div>
-                        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                        <div classNme="title">New BEER</div>
+                        <div classNme="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
                     </div>
             </div>
         
